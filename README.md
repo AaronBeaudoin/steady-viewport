7aa6e5b2dccbba3b0ca5973f96170b1850a80749
Aaron Beaudoin <aaron.beaudoin@aryse.com>
2022-02-02 16:44:01 -0600

    Added layout features and partially updated README

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # ⛰ `steady-viewport`
 
-The `steady-viewport` JavaScript utility is a tiny, dead simple script with no dependencies which introduces two CSS variables that can be used instead of the CSS `vh` and `vw` units to size elements based on the viewport.
+The `steady-viewport` JavaScript utility is a tiny, dead simple script with no dependencies which introduces CSS variables that can be used instead of the CSS `vh` and `vw` units to size elements based on the viewport. It can also optionally add super useful CSS variables to help size element which should take up the height of the viewport minus some top navigation bar and/or banner elements.
 
-It was born from the frustrating handling of the `vw` and `vh` units in mobile browsers with URL nagivation bars that toggle in response to user scrolling and in desktop browsers on platforms that cause scrollbars to take up space on the page.
+It was born from the frustrating handling of the `vw` and `vh` units in mobile browsers with URL nagivation bars that toggle in response to user scrolling and in desktop browsers on platforms that cause scrollbars to take up space on the page. It also aims to resolve the frustration of trying to size elements without defining fixed heights for elements at the top of your layout (more on this below).
+
 
 ## Installation
 
@@ -12,7 +13,7 @@ For now, this utility is not on `npm`, but it's still easy to install using the 
 npm install https://github.com/AaronBeaudoin/steady-viewport.git
 ```
 
-Then, include it in your HTML at the very top of the head so it runs first:
+Then, include it in your HTML **at the very top of the `<body>`** so it runs before it is parsed:
 
 ```html
 <script src="/path/to/script"></script>
@@ -24,17 +25,36 @@ For example, in Vite you can add the following:
 <script src="/node_modules/steady-viewport"></script>
 ```
 
-Note that `type="module"` is intentionally left out, because this would defeat the purpose of putting the script at the top of the `<head>`. Since major layout element may depend on these variables, the script needs to run as soon as possible to avoid layout shift as the page is loading.
+Note that `type="module"` is intentionally left out, because this would defeat the purpose of putting the script in any particular location since such scripts are deferred. Since major layout element may depend on these variables, the script is best run before the `<body>` is rendered to avoid layout shift as the page is loading.
+
+Finally, add the following right below the script you just included to enable the features you want:
+
+```html
+<script>steady(["viewport", "layout"]);</script>
+```
+
+If you do not intend to use the layout CSS variables added by `steady-viewport`, you can remove `"layout"` from the list and the variables will not be created. Note that the layout features of `steady-viewport` depend on `ResizeObserver`, which is only supported in all major browsers as of Q1 2020.
 
 
-## CSS Variables Added
+## Usage (Viewport CSS Variables)
 
-Only two simple CSS variables are added and updated by this utility:
+Two simple CSS variables are added and updated by this utility when the `"viewport"` feature is enabled:
 
 - `--vw`: The viewport width, **not** including the vertical scrollbar if present.
 - `--vh`: The viewport height, **not** including the horizontal scrollbar if present, and not affected by insignificant changes to the viewport height—such as the URL navigation bar toggling in a mobile browser.
 
-That's it. Note that `--vw` is the same as `100vw`, not `1vw`. The same goes for `--vh`.
+Simply use them anywhere you would use `100vw` or `100vh`.
+
+
+## Usage (Layout CSS Variables)
+
+Two more simple CSS variables are added and updated by this utility when the `"layout"` feature is enabled:
+
+- `--ph`: blah blah
+- `--th`: blah blah
+
+blah blah blah
+
 
 ## Why not just use `vh` and `vw`?
 
@@ -45,6 +65,7 @@ When you try to layout things in CSS based on the size of the viewport, there ar
 - Relying on the `vw` and `vh` units to size elements that should be the width or height of the page is not a viable solution for browsers where the scrollbar takes up space on the page (it is not overlaid). This is because the `vw` and `vh` units includes the vertical and horizontal scrollbars respectively. This causes elements with dimensions relying on the `vw` or `vh` units to potentially either introduce unwanted (most likely horizontal) scrolling or be a bit too tall, both of which negatively impact the user experience.
 
 The fact that these two issues even exist really makes the `vh` and `vw` units feel half-baked. In reality, we can blame varying browser implementations or the emergence of new types of devices for these issues, but either way they're probably not going to change. Since sizing elements based on the viewport is still a really useful feature regardless, this utility is helpful because it solves both of these issues without negatively impacting any use cases as far as I know.
+
 
 ## Caveat to `vh` Change Detection
 
@@ -61,6 +82,7 @@ To recap, the following three conditions must all be met:
 As previously indicated, I haven't been able to think of a scenario where this would be a problem, but it's good to be aware of. From my testing, these conditions are enough to prevent the viewport height from updating in response to small changes like toggling the URL navigation bar; but also narrow enough to _not_ block updating in response to changing a device's orientation, dragging the corner of a window with a cursor, or other changes which change the dimensions of the viewport more significantly.
 
 It is therefore being assumed that any smaller changes which do not cause this utility to update the `--vh` CSS variable are—like the URL navigation bar—insignficant enough to be not worth responding to. If you find an exception to this, please let me know!
+
 
 ### Author Information
 
--- index.js
+++ index.js
@@ -1,49 +1,108 @@
-let lastHandlerTimestamp = null;
-let lastWindowViewportPortrait = null;
-let lastWindowViewportArea = null;
-let lastViewportWidth = null;
-let lastViewportHeight = null;
+function steady(features) {
+  function steadyViewport() {
+    window.viewportWidth = null;
+    window.viewportHeight = null;
 
-let eventHandler = _ => {
-  let root = document.querySelector(":root");
-  let windowViewportPortrait = window.innerWidth < window.innerHeight;
-  let windowViewportArea = window.innerWidth * window.innerHeight;
+    let lastHandlerTimestamp = null;
+    let lastWindowViewportPortrait = null;
+    let lastWindowViewportArea = null;
 
-  // We avoid updating the viewport height in circumstances where it is likely due to a toggling URL bar.
-  // For the viewport height to not be updated, the following 3 conditions must be met:
-  let shouldUpdateViewportHeight = !(
+    let eventHandler = _ => {
+      let root = document.querySelector(":root");
+      let windowViewportPortrait = window.innerWidth < window.innerHeight;
+      let windowViewportArea = window.innerWidth * window.innerHeight;
 
-    // The handler hasn't been called in the last half second.
-    // (If it has, then the user is probably dragging the corner of the window to resize it.)
-    (new Date() - lastHandlerTimestamp) > 500 &&
-    
-    // The orientation of the viewport hasn't changed.
-    // (An orientation change is always significant enough to justify updating the viewport height.)
-    windowViewportPortrait === lastWindowViewportPortrait &&
-  
-    // The area of the viewport has changed by less than 1/3 of it's last area.
-    // (No URL navigation bar I know of takes up enough of the screen to cause a change larger than this).
-    Math.abs(windowViewportArea - lastWindowViewportArea) < (lastWindowViewportArea / 3)
-  );
-    
-  if (root.clientWidth !== lastViewportWidth) {
-    // `root.clientWidth` is the viewport width without the vertical scrollbar.
-    root.style.setProperty("--vw", `${root.clientWidth}px`);
-    lastViewportWidth = root.clientWidth;
+      // We avoid updating the viewport height in circumstances where it is likely due to a toggling URL bar.
+      // For the viewport height to not be updated, the following 3 conditions must be met:
+      let shouldUpdateViewportHeight = !(
+
+        // The handler hasn't been called in the last half second.
+        // (If it has, then the user is probably dragging the corner of the window to resize it.)
+        (new Date() - lastHandlerTimestamp) > 500 &&
+        
+        // The orientation of the viewport hasn't changed.
+        // (An orientation change is always significant enough to justify updating the viewport height.)
+        windowViewportPortrait === lastWindowViewportPortrait &&
+      
+        // The area of the viewport has changed by less than 1/3 of it's last area.
+        // (No URL navigation bar I know of takes up enough of the screen to cause a change larger than this).
+        Math.abs(windowViewportArea - lastWindowViewportArea) < (lastWindowViewportArea / 3)
+      );
+        
+      if (root.clientWidth !== viewportWidth) {
+        // `root.clientWidth` is the viewport width without the vertical scrollbar.
+        root.style.setProperty("--vw", `${root.clientWidth}px`);
+        viewportWidth = root.clientWidth;
+      }
+      
+      if (root.clientHeight !== viewportHeight && shouldUpdateViewportHeight) {
+        // `root.clientHeight` is the viewport height without the horizontal scrollbar.
+        root.style.setProperty("--vh", `${root.clientHeight}px`);
+        viewportHeight = root.clientHeight;
+
+        lastWindowViewportPortrait = windowViewportPortrait;
+        lastWindowViewportArea = windowViewportArea;
+      }
+
+      // Always update the timestamp of the last time this handler was called.
+      lastHandlerTimestamp = new Date();
+    };
+
+    eventHandler();
+    window.addEventListener("load", eventHandler);
+    window.addEventListener("resize", eventHandler);
   }
-  
-  if (root.clientHeight !== lastViewportHeight && shouldUpdateViewportHeight) {
-    // `root.clientHeight` is the viewport height without the horizontal scrollbar.
-    root.style.setProperty("--vh", `${root.clientHeight}px`);
-    lastViewportHeight = root.clientHeight;
-    lastWindowViewportPortrait = windowViewportPortrait;
-    lastWindowViewportArea = windowViewportArea;
+
+  function steadyLayout() {
+    // `ResizeObserver` is required in order to detect updates to the layout.
+    if (typeof window.ResizeObserver !== undefined) {
+
+      let resizeObserver = null;
+      let mutationObserver = null;
+      let topElements = [];
+
+      let resizeObserverHandler = resizes => {
+        let stickyHeight = 0;
+        let topHeight = 0;
+
+        resizes.forEach(resize => {
+          if (resize.target.dataset.steady === "sticky") stickyHeight += resize.target.offsetHeight;
+          if (resize.target.dataset.steady === "top") topHeight += resize.target.offsetHeight;
+        });
+
+        let root = document.querySelector(":root");
+        root.style.setProperty("--ph", `calc(var(--vh) - ${stickyHeight}px)`);
+        root.style.setProperty("--th", `calc(var(--vh) - ${stickyHeight}px - ${topHeight}px)`);
+      };
+
+      let mutationHandler = mutation => {
+        mutation.addedNodes.forEach(node => {
+          if (node.nodeType !== Node.ELEMENT_NODE) return;
+          if (!node.dataset.steady) return;
+          topElements.push(node);
+        });
+      };
+
+      let mutationObserverHandler = mutations => {
+        mutations.forEach(mutationHandler);
+        topElements.forEach(_ => resizeObserver.observe(_));
+      };
+
+      resizeObserver = new ResizeObserver(resizeObserverHandler);
+      mutationObserver = new MutationObserver(mutationObserverHandler);
+
+      // Layout "steady" elements must be direct children of `body`.
+      mutationObserver.observe(document.body, { childList: true });
+
+    // If `ResizeObserver` is not supported, just use some naive sensible defaults.
+    } else {
+
+      let root = document.querySelector(":root");
+      root.style.setProperty("--ph", `calc(var(--vh) * 0.85)`);
+      root.style.setProperty("--th", `calc(var(--vh) * 0.65)`);
+    }
   }
 
-  // Always update the timestamp of the last time this handler was called.
-  lastHandlerTimestamp = new Date();
-};
-
-eventHandler();
-window.addEventListener("load", eventHandler);
-window.addEventListener("resize", eventHandler);
+  if (features.includes("viewport")) steadyViewport();
+  if (features.includes("layout")) steadyLayout();
+}
